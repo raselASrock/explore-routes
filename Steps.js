0e2ac9eacc6dbb8c93987dc8011c3d8ede439761
raselASrock <mdraselm413@gmail.com>
2022-10-05 00:40:26 +0600

    react-router

--- Steps.js
+++ Steps.js
@@ -1,4 +1,7 @@
 /* 
 01. install react router: npm install react-router-dom
+02. create a router using createBrowserRouter
+03. add routerProvider and pass router props
+04. Create some routes inside the router
 
 */
