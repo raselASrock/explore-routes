5f3740b6a784426b2a0a59dbcdaddaab90cde732
raselASrock <mdraselm413@gmail.com>
2022-10-05 00:57:08 +0600

    link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 function App() {
   const router = createBrowserRouter([
     {path: '/', element: <div>This is default page</div>},
-    {path: '/home',element: <div>Home Page</div>},
-    {path: '/about', element: <div>About Us</div>}
+    {path: '/home',element: <Home></Home>},
+    {path: '/about', element: <About></About>}
   ])
   return (
     <div className="App">
