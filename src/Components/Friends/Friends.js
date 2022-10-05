487ae687f37db1092e40ee7eb10214d48d49feae
raselASrock <mdraselm413@gmail.com>
2022-10-05 19:31:13 +0600

    friends

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Friends = () => {
+    const friends = useLoaderData()
     return (
         <div>
-            <h2>I have so many friends.</h2>
+            <h2>I have so many friends.{friends.length}</h2>
         </div>
     );
 };
