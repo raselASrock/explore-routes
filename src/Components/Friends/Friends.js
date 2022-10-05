cea6a077ff937e71ddcc6aa67cc06f8c7cf7981b
raselASrock <mdraselm413@gmail.com>
2022-10-05 19:49:29 +0600

    friend-style

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
-    const friends = useLoaderData()
+    const friends = useLoaderData();
+    console.log(friends);
     return (
         <div>
             <h2>I have so many friends.{friends.length}</h2>
+            {
+                friends.map(friend => <Friend
+                key={friend.id}
+                friend = {friend}
+                ></Friend>)
+            }
         </div>
     );
 };
